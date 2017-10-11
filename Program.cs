1905bda003408ccdc7eda03debfcb77de54f6523
FIC <sandrounib@hotmail.com>
2017-10-10 21:51:39 -0300

    Criando Arquivos

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace semana2_aula3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo fi = new FileInfo("Arquivo.csv");
+            Console.WriteLine("Digite seu nome: ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite sua idade:");
+            string idade = Console.ReadLine();     
+            StreamWriter sw = new StreamWriter("Arquivo.csv",true);
+            sw.WriteLine(nome + ";" + idade);
+            sw.Close();            
+        }
+    }
+}
